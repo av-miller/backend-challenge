0e2cf6b7b1878c7859eda61f907388d59374eb66
Andrew Miller <avmiller@outlook.com>
2022-12-23 16:11:48 -0500

    adding database migration scripts

--- config/dependency.py
+++ config/dependency.py
@@ -4,7 +4,6 @@ from sqlalchemy import create_engine
 from config.settings import Settings
 from dao.factory_dao import FactoryDao
 from dao.sprocket_dao import SprocketDao
-from orm import Base
 
 
 def get_settings():
@@ -13,7 +12,6 @@ def get_settings():
 
 def get_sql_engine(settings=Depends(get_settings)):
     engine = create_engine(settings.db_url)
-    Base.metadata.create_all(engine)
     return engine
 
 
